8822b3b0e65c90f0b6587332bf5ed5f292e7fa60
Lucas Sachet <lucasachet@gmail.com>
2021-05-01 18:35:40 -0300

    feat: add detail

--- src/App.js
+++ src/App.js
@@ -1,6 +1,13 @@
 import React from 'react';
 import Header from './components/Header'
 import Home from './components/Home';
+import Detail from './components/Detail';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 
 import './App.css';
@@ -8,8 +15,18 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <Header />
-      <Home />
+      <Router>
+       <Header />
+        <Switch>
+          <Route path="/detail">
+            <Detail />
+          </Route>
+          <Route path="/">
+           <Home />
+          </Route>
+        </Switch>
+
+      </Router>
     </div>
   );
 }
---
+++ src/components/Detail.js
@@ -0,0 +1,149 @@
+import React from 'react'
+import styled from 'styled-components'
+
+function Detail() {
+  return (
+    <Container>
+      <Background>
+        <img src="https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/FEBDDA12676B9FEBF496FA2C25D93C284F1624DAB8BB14EDB43FABEF0ABD58C7/scale?width=1200&aspectRatio=1.78&format=jpeg" alt="bg-detail"/>
+      </Background>
+
+      <ImageTitle>
+        <img src="https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/679119E881B53C0ED9485B0A245963AB9E428F65A165908BC64C1AFC46C4D8C6/scale?width=500&aspectRatio=1.78" alt="title-img"/>
+      </ImageTitle>
+
+      <Controls>
+        <PlayButton>
+          <img src="/images/play-icon-black.png" alt="play"/>
+          <span>PLAY</span>
+        </PlayButton>
+        <TrailerButton>
+          <img src="/images/play-icon-white.png" alt="trailer"/>
+          <span>TRAILER</span>
+        </TrailerButton>
+        <AddButton>
+          <span>+</span>
+        </AddButton>
+        <GroupWatchButton>
+          <img src="/images/group-icon.png" alt="group"/>
+        </GroupWatchButton>
+      </Controls>
+      <Subtitle>
+       2021 &#8226; 20m &#8226; Family, Fantasy, Animation
+      </Subtitle>
+      <Description>
+        A star wars movie to play A star wars movie to play A star wars movie to play
+      </Description>
+    </Container>
+  )
+}
+
+export default Detail
+
+const Container = styled.div`
+min-height: calc(100vh -70px);
+padding: 0 calc(3.5vw + 5px);
+position: relative;
+`
+
+const Background = styled.div`
+  position: fixed;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  opacity: 0.8;
+
+  z-index: -1;
+
+  img {
+    width: 100%;
+    height:100%;
+    object-fit: cover;
+  }
+`
+const ImageTitle = styled.div`
+  min-height: 170px;
+  min-width: 200px;
+  height: 30vh;
+  width: 35vw;
+
+  img {
+    width: 100%;
+    height: 100%;
+    object-fit: contain;
+  }
+`
+
+const Controls = styled.button`
+  display: flex;
+  align-items: center;
+
+  background: transparent;
+  border: none;
+
+`
+
+const PlayButton = styled.button`
+  cursor: pointer;
+  border-radius: 4px;
+  font-size: 15px;
+  letter-spacing: 1.8px;
+  padding: 0px 24px;
+  margin-right: 22px;
+  height: 56px;
+  background: rgb(249,249,249);
+  border: none;
+
+  display: flex;
+  align-items: center;
+
+  &:hover {
+    background: rgb(218, 218, 218);
+  
+  }
+`
+
+const TrailerButton = styled(PlayButton)`
+  background: rgba(0, 0, 0, 0.3);
+  border: 1px solid rgb(249, 249, 249);
+  color: rgb(249, 249, 249);
+
+`
+
+const AddButton = styled.button`
+  cursor: pointer;
+  width: 44px;
+  height: 44px;
+  margin-right: 16px;
+  border: 2px solid rgb(255, 255, 255);
+  border-radius: 50%;
+  background-color: rgba(0, 0, 0, 0.6);
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  span {
+    font-size: 30px;
+    color: rgb(255, 255, 255);
+  }
+`
+
+const GroupWatchButton = styled(AddButton)`
+  background-color: rgb(0, 0, 0);
+`
+
+const Subtitle = styled.div`
+  color: rgb(249, 249, 249);
+  font-size: 15px;
+  min-height: 20px;
+  margin-top: 26px;
+`
+
+const Description = styled.div`
+  color: rgb(249, 249, 249);
+  line-height: 1.4px;
+  font-size: 20px;
+  margin-top: 16px;
+`
